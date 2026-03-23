24594f3bf431081246ddee6bee92af2e916b4839
Snaijder Vargas <snaijdervargas46@gmail.com>
2026-03-23 15:40:19 -0600

    Primera parte de evaluacion 23/3/26

---
+++ nonito-web/src/assets/views/Pagina404.jsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col, Button, } from "react-bootstrap";
+
+const Pagina404 = () => {
+  return (
+    <Container className="mt-3">
+      <Row className="align-items-center">
+        <Col>
+          <h2><i className="bi bi-house-fill me-2"></i> Inicio</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Pagina404;
